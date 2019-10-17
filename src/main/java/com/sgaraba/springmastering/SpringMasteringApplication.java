5318a7939604d21f40fc72bf6d706efd287e92b7
sgaraba <sergiu.garaba@esempla.com>
2019-10-17 15:02:12 +0300

    First commit

---
+++ src/main/java/com/sgaraba/springmastering/SpringMasteringApplication.java
@@ -0,0 +1,13 @@
+package com.sgaraba.springmastering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMasteringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMasteringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sgaraba/springmastering/SpringMasteringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sgaraba.springmastering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMasteringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
