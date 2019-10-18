10f73951f2d5dbeea3114a87dce58a76cba21679
sgaraba <sergiu.garaba@esempla.com>
2019-10-18 12:05:51 +0300

    Add ExceptionHandler Controller

--- src/main/java/com/sgaraba/springmastering/controller/TodoController.java
+++ src/main/java/com/sgaraba/springmastering/controller/TodoController.java
@@ -1,14 +1,19 @@
 package com.sgaraba.springmastering.controller;
 
 import com.sgaraba.springmastering.bean.Todo;
+import com.sgaraba.springmastering.exception.NotFoundException;
 import com.sgaraba.springmastering.service.TodoService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
 import java.util.List;
+import java.util.Objects;
+
 
 @RestController
 @RequiredArgsConstructor
@@ -23,7 +28,16 @@ public class TodoController {
 
     @GetMapping(path = "/users/{name}/todos/{id}")
     public Todo retrieveTodo(@PathVariable String name, @PathVariable int id) {
-        return todoService.retrieveTodo(id);
+        final Todo todo = todoService.retrieveTodo(id);
+        if (Objects.isNull(todo)) {
+            throw new NotFoundException("Todo Not Found.");
+        }
+        return todo;
+    }
+
+    @GetMapping(path = "/users/dummy-service")
+    public Todo errorService() {
+        throw new RuntimeException("Some Exception Occured");
     }
 
     @PostMapping("/users/{name}/todos")
