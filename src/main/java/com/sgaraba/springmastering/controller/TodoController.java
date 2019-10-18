39981ad8b8355ab7b286afc8c5e39d3f5ceb3d36
sgaraba <sergiu.garaba@esempla.com>
2019-10-18 15:12:41 +0300

    Config swagger

--- src/main/java/com/sgaraba/springmastering/controller/TodoController.java
+++ src/main/java/com/sgaraba/springmastering/controller/TodoController.java
@@ -3,9 +3,8 @@ package com.sgaraba.springmastering.controller;
 import com.sgaraba.springmastering.bean.Todo;
 import com.sgaraba.springmastering.exception.NotFoundException;
 import com.sgaraba.springmastering.service.TodoService;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
-import org.springframework.hateoas.EntityModel;
-import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -15,28 +14,35 @@ import java.net.URI;
 import java.util.List;
 import java.util.Objects;
 
-
 @RestController
 @RequiredArgsConstructor
 public class TodoController {
 
     private final TodoService todoService;
 
+    @ApiOperation(
+            value = "Retrieve all todos for a user by passing in his name",
+            notes = "A list of matching todos is returned. Currently pagination is not supported.",
+            response = Todo.class,
+            responseContainer = "List",
+            produces = "application/json"
+    )
     @GetMapping("/users/{name}/todos")
     public List<Todo> retrieveTodos(@PathVariable String name) {
         return todoService.retrieveTodos(name);
     }
 
     @GetMapping(path = "/users/{name}/todos/{id}")
-    public EntityModel<Todo> retrieveTodo(@PathVariable String name, @PathVariable int id) {
+    public Todo retrieveTodo(@PathVariable String name, @PathVariable int id) {
         final Todo todo = todoService.retrieveTodo(id);
         if (Objects.isNull(todo)) {
             throw new NotFoundException("Todo Not Found.");
         }
-        EntityModel<Todo> todoResource = new EntityModel<Todo>(todo);
+        return todo;
+        /*EntityModel<Todo> todoResource = new EntityModel<Todo>(todo);
         WebMvcLinkBuilder linkTo = WebMvcLinkBuilder.linkTo(WebMvcLinkBuilder.methodOn(this.getClass()).retrieveTodos(name));
         todoResource.add(linkTo.withRel("parent"));
-        return todoResource;
+        return todoResource;*/
     }
 
     @GetMapping(path = "/users/dummy-service")
