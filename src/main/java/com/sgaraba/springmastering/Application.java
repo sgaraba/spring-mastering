91f6df14eea0af8c49ebd150d42107f04be333af
sgaraba <sergiu.garaba@esempla.com>
2019-10-18 22:32:41 +0300

    Enable caching

--- src/main/java/com/sgaraba/springmastering/Application.java
+++ src/main/java/com/sgaraba/springmastering/Application.java
@@ -2,6 +2,7 @@ package com.sgaraba.springmastering;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
@@ -9,6 +10,7 @@ import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
+@EnableCaching
 @SpringBootApplication
 public class Application {
 
